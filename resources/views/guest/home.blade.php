1de4789ca19b5085ed95c317b1432b21983f46eb
MicheleIanni86 <micheleianni86@gmail.com>
2024-04-04 22:37:27 +0200

    feat: Added Dark-Mode

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="jumbotron p-5 mb-4 bg-light rounded-3">
-    <div class="container py-5 text-center" >
+
+    <div class="container py-5 text-center mt-5" >
         <div class="logo_laravel">
             <img src="https://i.ibb.co/7XB4vzx/8b1e1849-3429-43cb-9317-67a299e00df7.jpg" alt="8b1e1849-3429-43cb-9317-67a299e00df7" border="0" />
         </div>
@@ -12,7 +12,7 @@
 
    
     </div>
-</div>
+
 
 <div class="content">
     <div class="container">
--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-dark text-white">
+<footer class="bg-secondary text-white">
   <div class="container py-4">
     &copy; {{ env('APP_AUTHOR', 'Michele Ianni') }} {{ now()->year }}
   </div>

b103c9318b3ab83947b598ccb2afbfcad8e27744
MicheleIanni86 <micheleianni86@gmail.com>
2024-04-04 02:19:11 +0200

    feat: Printed List Project

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,9 +1,37 @@
 @extends('layouts.app')
 
+@section('title', 'Lista Projects')
+
 @section('content')
 <section>
-    <div class="container">
+    <div class="container my-4">
         <h1>Lista Projects</h1>
+
+        <table class="table">
+            <thead>
+                <th>TITOLO</th>
+                <th>SLUG</th>
+                <th>ESTRATTO</th>
+                <th></th>
+            </thead>
+            <tbody>
+                @forelse ($projects as $project)
+                <tr>
+                    <td>{{ $project['title'] }}</td>
+                    <td>{{ $project['slug'] }}</td>
+                    <td>{{ $project['content'] }}</td>
+                    <td></td>
+                </tr>
+                    
+                @empty
+                    <tr>
+                        <td colspan="100%">
+                            <i>Nessun risultato trovato</i>
+                        </td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
 
     </div>
 </section>
